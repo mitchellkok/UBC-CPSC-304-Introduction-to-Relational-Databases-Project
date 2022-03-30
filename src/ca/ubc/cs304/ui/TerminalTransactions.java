668c3d1d0ff1b6f797101ecd45b7b37e11f69c1f
Lok Fung Shuen <fslok@connect.ust.hk>
2022-03-30 15:52:52 -0700

    Added Insert Player function

--- src/ca/ubc/cs304/ui/TerminalTransactions.java
+++ src/ca/ubc/cs304/ui/TerminalTransactions.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 
 import ca.ubc.cs304.delegates.TerminalTransactionsDelegate;
 import ca.ubc.cs304.model.BranchModel;
+import ca.ubc.cs304.model.PlayersModel;
 
 /**
  * The class is only responsible for handling terminal text inputs. 
@@ -98,8 +99,24 @@ public class TerminalTransactions {
 						if (choice2 != INVALID_INPUT) {
 							switch(choice2){
 								case 1:
-
-
+									handleInsertPlayerOption();
+									break;
+								case 2:
+									break;
+								case 3:
+									break;
+								case 4:
+									break;
+								case 5:
+									break;
+								case 6:
+									break;
+								case 7:
+									handleQuitOption();
+									break;
+								default:
+									System.out.println(WARNING_TAG + " The number that you entered was not a valid option.");
+									break;
 							}
 						}
 					}
@@ -125,8 +142,74 @@ public class TerminalTransactions {
 		}		
 	}
 
+	// Players
 	private void handleInsertPlayerOption(){
+		int jerseynumber = INVALID_INPUT;
+		while (jerseynumber == INVALID_INPUT) {
+			System.out.print("Please enter the jersey number you wish to insert: ");
+			jerseynumber = readInteger(false);
+		}
 
+		String tname = null;
+		while (tname == null || tname.length() <= 0) {
+			System.out.print("Please enter the team name you wish to insert: ");
+			tname = readLine().trim();
+		}
+
+		String city = null;
+		while (city == null || city.length() <= 0) {
+			System.out.print("Please enter the city you wish to insert: ");
+			city = readLine().trim();
+		}
+
+		String pname = null;
+		while (pname == null || pname.length() <= 0) {
+			System.out.print("Please enter the player name you wish to insert: ");
+			pname = readLine().trim();
+		}
+
+		// height is allowed to be null so we don't need to repeatedly ask for the address
+		System.out.print("Please enter the height you wish to insert (enter 0 if you wish to leave it empty): ");
+		int height = readInteger(false);
+
+		// weight is allowed to be null so we don't need to repeatedly ask for the address
+		System.out.print("Please enter the height you wish to insert (enter 0 if you wish to leave it empty): ");
+		int weight = readInteger(false);
+
+		// age is allowed to be null so we don't need to repeatedly ask for the address
+		System.out.print("Please enter the height you wish to insert (enter -1 if you wish to leave it empty): ");
+		int age = readInteger(false);
+
+		int clicensenumber = INVALID_INPUT;
+		while (clicensenumber == INVALID_INPUT) {
+			System.out.print("Please enter the coach license number you wish to insert: ");
+			clicensenumber = readInteger(false);
+		}
+
+		PlayersModel model = new PlayersModel(
+				jerseynumber,
+				tname,
+				city,
+				pname,
+				height,
+				weight,
+				age,
+				clicensenumber);
+		delegate.insertPlayer(model);
+	}
+
+	private void handleDeletePlayerOption() {
+
+		/**
+		int branchId = INVALID_INPUT;
+		while (branchId == INVALID_INPUT) {
+			System.out.print("Please enter the branch ID you wish to delete: ");
+			branchId = readInteger(false);
+			if (branchId != INVALID_INPUT) {
+				delegate.deleteBranch(branchId);
+			}
+		}
+		 **/
 	}
 	
 	private void handleDeleteOption() {
